ba2b67a1ccd8cde68452934a41bb5f6956990d51
ozwas <163393194+ozwas@users.noreply.github.com>
2025-04-09 12:19:14 +0400

    Commit 9 - basic script for game with random choice

--- script.js
+++ script.js
@@ -7,3 +7,48 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+/* https://stackoverflow.com/questions/15653145/using-google-text-to-speech-in-javascript */
+function say(m) {
+    let msg = new SpeechSynthesisUtterance(m);
+    speechSynthesis.speak(msg);
+}
+
+document.getElementById("rockButton").addEventListener("click", function(event) {      
+    console.log(event.target.id);      
+    play("rock");    
+});
+
+document.getElementById("paperButton").addEventListener("click", function(event) {      
+    console.log(event.target.id);      
+    play("paper");    
+});
+
+document.getElementById("scissorsButton").addEventListener("click", function(event) {      
+    console.log(event.target.id);      
+    play("scissors");    
+});
+
+/* Rock Paper Scissors game with random computer choices*/
+function play(user) {
+    // Computer's random choice
+    const choices = ["rock", "paper", "scissors"];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+    // Determine the result of the game
+    let result;
+    if (user === computerChoice) {
+        result = "tie";
+    } else if (
+        (user === "rock" && computerChoice === "scissors") ||
+        (user === "paper" && computerChoice === "rock") ||
+        (user === "scissors" && computerChoice === "paper")
+    ) {
+        result = "win";
+    } else {
+        result = "lose";
+    }
+    let myMessage = "Computer chose " + computerChoice + ", you " + result + "!"
+    alert(myMessage);
+    say(myMessage);
+}
